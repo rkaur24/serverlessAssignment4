8313565be678da55f4816cfa9308f21086764513
Ramanpreet Kaur <sidhurimmy24@gmail.com>
2018-03-21 20:06:13 +0000

    first commit

---
+++ README.txt
@@ -0,0 +1,6 @@
+
+To test this assignment,
+run sls deploy
+
+Add/Delete files in S3 to see changes in dynamodb
+
---
+++ upload.js
@@ -0,0 +1,31 @@
+'use strict';
+const AWS = require('aws-sdk'); 
+const uuid = require('uuid');
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const s3 = new AWS.S3();
+
+module.exports.upload = (event, context, callback) => {
+  event.Records.forEach((record) => {
+    const file = record.s3.object.key;
+    const opType = record.eventName
+    const opTime = record.eventTime
+    
+    const params = {
+      TableName: 'mydropboxrecord',
+      Item: {
+        id: uuid.v1(),
+        fileName: file,
+        opType: opType,
+        opTime: opTime
+      }
+    }
+
+    dynamoDb.put(params, (err, result) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+    });
+
+});
+};
